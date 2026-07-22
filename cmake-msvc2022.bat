f7e7a6b071e32c5f261fdd62d01b2bf1fcbacace
cwboyd <cwboyd@users.noreply.github.com>
2026-07-22 14:01:03 -0500

    Fix cmake generate invoke batch file to pass VCPKG_TARGET_TRIPLET and VCPKG_HOST_TRIPLET, both of which appear to be necessary

--- cmake-msvc2022.bat
+++ cmake-msvc2022.bat
@@ -1,1 +1,2 @@
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/vcpkg/scripts/buildsystems/vcpkg.cmake" 
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DVCPKG_HOST_TRIPLET=x64-windows-static
+
